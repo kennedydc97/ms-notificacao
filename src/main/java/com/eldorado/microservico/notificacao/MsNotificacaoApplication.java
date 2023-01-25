3b6cf247573d257c5c4e3186e25fec09876e5423
Kennedy <kennedy_dourado@hotmail.com>
2023-01-25 16:29:24 -0300

    commit inicial

---
+++ src/main/java/com/eldorado/microservico/notificacao/MsNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.microservico.notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificacaoApplication.class, args);
+	}
+
+}
